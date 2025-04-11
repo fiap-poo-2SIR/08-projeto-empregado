7268146facc9b5a7a7b6a4051c848a12b3322dbf
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2025-04-10 21:45:49 -0300

    estrutura inicial do projeto

---
+++ src/EmpregadoComissionado.java
@@ -0,0 +1,8 @@
+public class EmpregadoComissionado extends Empregado {
+    double totalDeVendas;
+    double comissao;
+
+    public double calcularSalario() {
+        return totalDeVendas * comissao / 100;
+    }
+}
